ef848fdad32472e8119dd7130d2d5d32c6b639a0
Jair Yara <jair.yara11@gmail.com>
2024-08-27 22:55:29 -0500

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'tic-tac-toe',
+  webDir: 'dist'
+};
+
+export default config;
